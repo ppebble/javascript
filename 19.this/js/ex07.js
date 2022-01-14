062dc8e8924639ba39b9bf98d0158bbd37fb9acf
pebble <ask135799@naver.com>
2022-01-14 16:59:35 +0900

    19

---
+++ 19.this/js/ex07.js
@@ -0,0 +1,33 @@
+ //tabMenu class 생성
+ function TabMenu(selector){ //생성자 부분에서 매개변수를 받아온다.
+    //property
+    this.$tabMenu = null;
+    this.$menItems = null;
+    this.$selectedMenuItem = null;
+    //해당 selecotr에 의해서 각기 다른 인스턴스를 만들기 위해 init()이 호출된다.
+    this.init(selector);
+    this.initEvent(selector);
+}
+    //prototype Method
+    TabMenu.prototype.init = function(selector){
+        this.$tabMenu = $(selector);
+        this.$menItems = this.$tabMenu.find("li");
+    }
+    TabMenu.prototype.initEvent = function(){
+        var objThis = this;
+        this.$menItems.on("click", function(){
+            objThis.setSelectItem($(this));//선택된 항목이 매개변수로 넘어간다.
+        })
+    }
+    TabMenu.prototype.setSelectItem = function($menuItem){
+        if(this.$selectedMenuItem){//기존에 선택되어진 메뉴의 select class 삭제
+            this.$selectedMenuItem.removeClass("select");
+        }
+        this.$selectedMenuItem = $menuItem;
+        this.$selectedMenuItem.addClass("select");
+    }
+$(document).ready(function(){
+    var tabMenu1 = new TabMenu("#tabMenu1");
+    var tabMenu2 = new TabMenu("#tabMenu2"); //2개의 인스턴스 생성
+    var tabMenu3 = new TabMenu("#tabMenu3");
+})
